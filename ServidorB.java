e05c64c906aec9c84598932db4f3216ad6b8c838
Célio Silva <100172091+celio86@users.noreply.github.com>
2025-06-14 10:02:28 -0300

    Add files via upload

---
+++ ServidorB.java
@@ -0,0 +1,53 @@
+package servidorB;
+
+import java.io.*;
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+
+public class ServidorB {
+    public static void main(String[] args) throws IOException {
+        int porta = 6000; // mudar para 7000 no servidor C
+        ServerSocket serverSocket = new ServerSocket(porta);
+        System.out.println("Servidor B aguardando...");
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            new Thread(() -> tratarBusca(socket)).start();
+        }
+    }
+
+    public static void tratarBusca(Socket socket) {
+        try (
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        ) {
+            String entrada = in.readLine();
+            JSONObject req = new JSONObject(entrada);
+            String query = req.getString("query").toLowerCase();
+
+            JSONArray dados = new JSONArray(Files.readString(Paths.get("dados/dados_servidor_b.json"))); // alterar para parte2.json no C
+            JSONArray resultados = new JSONArray();
+
+            for (int i = 0; i < dados.length(); i++) {
+                JSONObject artigo = dados.getJSONObject(i);
+                String titulo = artigo.optString("title", "").toLowerCase();
+                String introducao = artigo.optString("abstract", "").toLowerCase();
+
+                if (titulo.contains(query) || introducao.contains(query)) {
+                    resultados.put(artigo);
+                }
+            }
+
+            out.write(resultados.toString());
+            out.newLine();
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
